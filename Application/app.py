c846a0f0a70b129d4b4726aeae23ed28912b310d
Lakshan Cooray <Lakshancooray23@gmail.com>
2024-07-23 20:05:56 +0530

    basic files created for css and js

--- Application/app.py
+++ Application/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    print("Lakshan Cooray")
+    print("Lakshan Cooray 123")
     return 'Hello World!'
 
 
