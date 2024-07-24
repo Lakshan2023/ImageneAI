5bf342d7359ab6b4259ec922e53cd3a0556b9e1e
Lakshan Cooray <Lakshancooray23@gmail.com>
2024-07-24 22:35:53 +0530

    pages links created

--- Application/app.py
+++ Application/app.py
@@ -1,13 +1,32 @@
 from flask import Flask
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     print("Lakshan Cooray 123")
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/why')
+def why():
+    return render_template('why.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/service')
+def sevice():
+    return render_template('service.html')
+
 
 
 if __name__ == '__main__':
+    print("Hello")
     app.run()
